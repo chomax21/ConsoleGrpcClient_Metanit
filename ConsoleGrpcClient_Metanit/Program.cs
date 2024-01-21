2932f067ef803c19a08a79c85fc10bb4fb77d879
chomax21 <maximcheb21@gmail.com>
2024-01-21 18:23:53 +0300

    Добавьте файлы проекта.

---
+++ ConsoleGrpcClient_Metanit/Program.cs
@@ -0,0 +1,11 @@
+﻿using Grpc.Net.Client;
+using GrpcServiceMetanitLearn;
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5291");
+
+var client = new Greeter.GreeterClient(channel);
+Console.WriteLine("Введите имя: ");
+var name = Console.ReadLine();
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = name});
+Console.WriteLine($"Ответ сервера {reply.Message}");
